957014572fddfbd8a90c191081e3983dd49c4cd7
kfoltyniak <kfoltyniak@software.altkom.pl>
2023-04-25 15:35:40 +0200

    email

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
-// console.log('foo foo foo Foo'.match(/foo/g)); //true
-// console.log('Foo'.match(/foo/i)); //true
-// console.log('foo'.match(/[fo]oo/g))
-// console.log('foo'.match(/[^1]oo/g))
-// console.log('foo'.match(/[a-zA-Z]/g))
-// console.log('foo'.match(/[^0-9]/g))
-// console.log('foo'.match(/[a-zA-Z]+/g))
-// console.log('94122910613'.match(/[0-9]{11}/g))
-// console.log('94122910613'.match(/[0-9]{11}/g))
-// console.log('ca.s'.match(/ca\.s/))
-
 // must be alphanumeric and contain 5 - 12 characters
 const isUserNameValid = (value) => !!value.match(/^[0-9]{5,12}$/);
-// must be alphanumeric and contain 5 - 12 characters
+// must be a valid telephone number (e.g. +48664609988)
 const isPhoneNumberValid = (value) => !!value.match(/^\+[0-9]{11}$/);
-
+// must contain only lowercase letters, numbers and hyphens and be 8 - 20 characters
+const isSlugValid = (value) => !!value.match(/^[a-z0-9-]{8,20}$/);
+// must be a valid address, e.g. me@mydomain.com
+const isEmailValid = (value) => !!value.match(/^[0-9a-z]+@[0-9a-z]+\.[a-z]{2,}$/);
 
 console.log(isUserNameValid('111111111'))
 console.log(isPhoneNumberValid('+48888123888'))
+console.log(isSlugValid('foo-bar-123'))
+console.log(isEmailValid('folt3k@gmail.com'))
