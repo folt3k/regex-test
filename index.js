eecc4abb56234481fac96a44b1d808d9b5902336
kfoltyniak <kfoltyniak@software.altkom.pl>
2023-04-25 15:12:32 +0200

    init

---
+++ index.js
@@ -0,0 +1,10 @@
+// console.log('foo foo foo Foo'.match(/foo/g)); //true
+// console.log('Foo'.match(/foo/i)); //true
+// console.log('foo'.match(/[fo]oo/g))
+// console.log('foo'.match(/[^1]oo/g))
+// console.log('foo'.match(/[a-zA-Z]/g))
+// console.log('foo'.match(/[^0-9]/g))
+// console.log('foo'.match(/[a-zA-Z]+/g))
+// console.log('94122910613'.match(/[0-9]{11}/g))
+// console.log('94122910613'.match(/[0-9]{11}/g))
+console.log('ca.s'.match(/ca\.s/))
