b130e5275f8d6fa8236709321f0e7c1ec3ca84c3
kfoltyniak <kfoltyniak@software.altkom.pl>
2023-04-25 15:19:47 +0200

    isusername valid

--- index.js
+++ index.js
@@ -7,4 +7,13 @@
 // console.log('foo'.match(/[a-zA-Z]+/g))
 // console.log('94122910613'.match(/[0-9]{11}/g))
 // console.log('94122910613'.match(/[0-9]{11}/g))
-console.log('ca.s'.match(/ca\.s/))
+// console.log('ca.s'.match(/ca\.s/))
+
+// must be alphanumeric and contain 5 - 12 characters
+const isUserNameValid = (value) => !!value.match(/^[0-9]{5,12}$/);
+// must be alphanumeric and contain 5 - 12 characters
+const isPhoneNumberValid = (value) => !!value.match(/^\+[0-9]{11}$/);
+
+
+console.log(isUserNameValid('111111111'))
+console.log(isPhoneNumberValid('+48888123888'))
